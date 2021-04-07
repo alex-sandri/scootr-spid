dd3df4ab882f07de1eea6001ee686d6106ac56ed
Alex Sandri <me@alexsandri.com>
2021-04-07 15:26:49 +0200

    Fix: logout script

--- logout.php
+++ logout.php
@@ -1,8 +1,36 @@
 <?php
 
-if (!$url = $sp->logoutPost(0)) {
+require_once(__DIR__ . "/vendor/autoload.php");
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$base = $_ENV["SP_ENTITYID"];
+$settings = [
+    "sp_entityid" => $base,
+    "sp_key_file" => "./sp_conf/sp.key",
+    "sp_cert_file" => "./sp_conf/sp.crt",
+    "sp_assertionconsumerservice" => [
+        $base . "/acs.php"
+    ],
+    "sp_singlelogoutservice" => [[$base . "/slo.php", ""]],
+    "sp_org_name" => "scootr",
+    "sp_org_display_name" => "scootr",
+    "idp_metadata_folder" => "./sp_conf/",
+    "sp_attributeconsumingservice" => [
+        [ "fiscalNumber" ],
+        [ "name", "familyName", "fiscalNumber", "email", "dateOfBirth" ]
+    ]
+];
+
+$sp = new \Italia\Spid\Sp($settings);
+
+if (!$url = $sp->logoutPost(0))
+{
     echo "Not logged in !<br>";
     echo "<a href=\"/\">Home</a>";
-} else {
+}
+else
+{
     echo $url;
 }
