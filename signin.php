a124d17f2d7f795c09e02192aeeafb826dfcc03a
Alex Sandri <me@alexsandri.com>
2021-04-12 19:52:19 +0200

    Redirect back to client if no idp is set in query params

--- signin.php
+++ signin.php
@@ -3,10 +3,14 @@
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/config/sp.php";
 
-if (isset($_GET) && isset($_GET["idp"]))
+if (!isset($_GET["idp"]) || empty($_GET["idp"]))
 {
-    $idp = $_GET["idp"];
+    header("Location: " . $_ENV["CLIENT_HOST"]);
+
+    exit;
 }
+
+$idp = $_GET["idp"];
 
 if (!$url = $sp->loginPost($idp, 0, 1, 2, null, true))
 {
