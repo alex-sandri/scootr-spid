6d739aa1de7843b8f0a6f7359f782fddd262ebd0
Alex Sandri <me@alexsandri.com>
2021-05-19 17:28:29 +0200

    Block signin requests for idp_testenv2 in prod

--- src/config/Config.php
+++ src/config/Config.php
@@ -1,6 +1,13 @@
 <?php
 
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
 class Config
 {
     const MIN_AGE = 18;
+
+    const IS_PRODUCTION = $_ENV["ENV"] === "prod";
 }
--- src/signin.php
+++ src/signin.php
@@ -11,6 +11,13 @@ if (!isset($_GET["idp"]) || empty($_GET["idp"]))
 
 $idp = $_GET["idp"];
 
+if (Config::IS_PRODUCTION && $idp === "idp_testenv2")
+{
+    http_response_code(403);
+
+    exit;
+}
+
 if (!$url = $sp->loginPost($idp, 0, 1, 2, null, true))
 {
     echo "Already logged in !<br>";
